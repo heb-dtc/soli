9a9759a192f707f75fa380e50d905adc6f01c365
Florent NOEL <florent.noel@protonmail.com>
2024-02-05 15:20:43 +0100

    Add necessary permission for android

--- composeApp/src/commonMain/kotlin/net.heb.soli/player/MiniPlayer.kt
+++ composeApp/src/commonMain/kotlin/net.heb.soli/player/MiniPlayer.kt
@@ -5,13 +5,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PauseCircle
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.PlayCircle
 import androidx.compose.material.icons.filled.Stop
+import androidx.compose.material.icons.filled.StopCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -27,28 +34,49 @@ fun MiniPlayer(player: Player) {
 @Composable
 fun MiniPlayer(state: PlayerState, onStop: () -> Unit, onPlay: () -> Unit) {
     Row(
-        modifier = Modifier.padding(4.dp)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
     ) {
-        Text(
-            text = "${state.item?.name}".uppercase(),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(8.dp)
-        )
+        Row(
+            modifier = Modifier.weight(1f)
+                .wrapContentWidth(Alignment.Start),
+        ) {
+            Text(
+                text = "${state.item?.name}".uppercase(),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(8.dp)
+            )
 
-        Icon(
-            imageVector = if (state.isPlaying) Icons.Filled.Stop else Icons.Filled.PlayArrow,
-            contentDescription = null,
-            modifier = Modifier.padding(8.dp)
-                .clickable {
-                    if (state.isPlaying) {
+        }
+        Row(
+            modifier = Modifier.weight(1f)
+                .wrapContentWidth(Alignment.End),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                imageVector = if (state.isPlaying) Icons.Filled.Pause else Icons.Filled.PlayArrow,
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
+                    .clickable {
+                        if (state.isPlaying) {
+                            onStop()
+                        } else {
+                            onPlay()
+                        }
+                    },
+            )
+
+            Icon(
+                Icons.Filled.Stop,
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
+                    .clickable {
                         onStop()
-                    } else {
-                        onPlay()
-                    }
-                },
-        )
+                    },
+            )
+        }
     }
 }
